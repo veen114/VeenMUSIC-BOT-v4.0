7ab577748ed7e880e600634d4ff08f32e3f21098
veen114 <81517566+veen114@users.noreply.github.com>
2024-08-14 10:24:39 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -77,21 +77,11 @@ client.login(process.env.TOKEN);
 module.exports = client;
 
 
-/*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
+/*        
 
 ╔════════════════════════════════════════════════════════════════════════╗
 ║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
+║  ## Created by Veeen14!                                                ║
 ║                                                                        ║
 ╚════════════════════════════════════════════════════════════════════════╝
 
